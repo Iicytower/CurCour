e04af0cb021113ade22c2097d420a3d282de750a
Iicytower <szymonfirkowicz@gmail.com>
2019-11-16 21:01:34 +0100

    first version with some arguments

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 const request = require('request');
 const colors = require('colors');
 
-if (process.argv.length === 2) {
+//help variables
+const arg = process.argv;
+
+if (arg.length <= 2) {
     console.log("You  didn't enter arguments. Please use this syntax:".yellow);
-    console.log("coucour <currency-code> <amount-of-money>".green);
+    console.log("coucour <argument> <currency-code> <amount-of-money>".green);
     console.log("For more information enter curcour -h".yellow);
     process.exit();
 }
 
+let table = '';
+if (arg[2] === "-m") table = 'a';
+else if (arg[2] === "-b" || arg[2] === "-a") table = 'c';
+else if (arg[2] === "-e") table = 'c';
+else if (arg[2] === "-g") table = 'c';
+else if (arg[2] === "-h") {
+    console.log("This is instruction of CurCour program".yellow);
+    console.log("Use this syntax: ");
+    console.log("coucour <argument> <currency-code> <amount-of-money>".green);
+    console.log(`Arguments:`);
+    console.log(`-m is middle price.`);
+    console.log(`-b is bid price price.`);
+    console.log(`-a is ask price price.`);
+    console.log(`-e is exchange rate.`);
+    console.log(`-g when u want to get amount of currency.`);
+    //here write instruction
+    process.exit();
+} else {
+    console.log("Wrong argument. Enter cucour -h for help".red);
+    process.exit();
+}
 
+
+
+const code = arg[3].toUpperCase();
+let amount = arg[4] || 0;
+
+//check is amount is a number
+
+if (/\D/gmi.test(amount)) {
+    console.log(`${amount}`.blue + ` is not a number. Enter integer.`.red);
+    console.log("For more information enter curcour -h".yellow);
+    process.exit();
+} else {
+    amount = parseInt(amount);
+}
+
+//validation of currency code
 const currencyCodes = [
     "THB",
     "USD",
@@ -46,25 +86,6 @@ const currencyCodes = [
     "CNY",
     "XDR"
 ];
-//help section
-if (process.argv[2] === "-h") {
-    console.log("This is instruction of CurCour program".yellow);
-    process.exit();
-}
-
-const code = process.argv[2].toUpperCase();
-let amount = process.argv[3] || 1;
-
-//check is amount is number
-
-if (/\D/gmi.test(amount)) {
-    console.log(`${amount} is not a number. Enter integer.`.red);
-    process.exit();
-} else {
-    amount = parseInt(amount);
-}
-
-//validation of currency code
 const isValidCurrencyCode = currencyCodes.find(currency => currency === code);
 if (!isValidCurrencyCode) {
     console.log(`Wrong currency. Here is list of correct currency:\n`.yellow);
@@ -72,9 +93,10 @@ if (!isValidCurrencyCode) {
     process.exit();
 }
 
-const url = `http://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`;
+const url = `http://api.nbp.pl/api/exchangerates/rates/${table}/${code}/?format=json`;
 
 request(url, { json: true }, (err, res, body) => {
+
     if (err) {
         console.log(`Error: \n${err}`.red);
         process.exit();
@@ -83,10 +105,43 @@ request(url, { json: true }, (err, res, body) => {
         console.log("Status code is diffrent then 200. Check URL adress.".red);
         process.exit();
     }
+    let message = '';
+    if (arg[2] === '-m') {
 
-    const count = amount / body.rates[0].mid;
-    const message =
-        `Middle price of ${body.code} is ${body.rates[0].mid} PLN\n`.green +
-        `For ${amount} PLN you will get ${Math.round(count * 100) / 100} ${body.code}`.blue;
+        const count = amount / body.rates[0].mid;
+        message =
+            `Middle price of ${body.currency} (${body.code}) is ${body.rates[0].mid} PLN\n`.green;
+        if (amount > 0) message = message + `For ${amount} PLN you will get ${Math.round(count * 100) / 100} ${body.code}`.blue;
+
+    } else if (arg[2] === '-a') {
+
+        const count = amount / body.rates[0].ask;
+        message =
+            `Ask price of ${body.currency} (${body.code}) is ${body.rates[0].ask} PLN\n`.green;
+        if (amount > 0) message = message + `For ${amount} PLN you will get ${Math.round(count * 100) / 100} ${body.code}`.blue;
+
+    } else if (arg[2] === '-b') {
+
+        const count = amount * body.rates[0].bid;
+        message =
+            `Bid price of ${body.currency} (${body.code}) is ${body.rates[0].bid} PLN\n`.green;
+        if (amount > 0) message = message + `For ${amount} ${body.code} you will get ${Math.round(count * 100) / 100} PLN`.blue;
+
+    } else if (arg[2] === '-g') {
+        console.log(amount, typeof amount);
+        if (amount = 100) console.log('dupa');
+
+        const count = amount * body.rates[0].ask;
+        message =
+            `Ask price of ${body.currency} (${body.code}) is ${body.rates[0].ask} PLN\n`.green +
+            `For get ${amount} ${body.code} you should pay ${Math.round(count * 100) / 100} PLN`.blue;
+
+    } else if (arg[2] === '-e') {
+
+        message = `Exchange rate of ${body.currency} (${body.code}) is:\n` +
+            `Buy: ${body.rates[0].bid}\n`.green +
+            `Sell: ${body.rates[0].ask}\n`.green +
+            `Spread: ${Math.round((body.rates[0].ask - body.rates[0].bid) * 1000000) / 1000000}`.yellow;
+    }
     console.log(message);
-})
+});
